489227bd260e5e8ac8b393971d772f7f0b1df70a
Chris Ardon <rockrisbl@gmail.com>
2021-10-10 04:42:10 -0600

    Creacion de los componentes

---
+++ src/screens/Multiplicacion.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Multiplicacion(){
+    return(
+        <View>
+            <Text>Estamos en Multiplicacion</Text>
+        </View>
+    );
+}
---
+++ src/screens/Sumatoria.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Sumatoria(){
+    return(
+        <View>
+            <Text>Estamos en Sumatoria</Text>
+        </View>
+    );
+}
