13195ceb77e65e64dd0c26e77550e917dc768150
Chris Ardon <rockrisbl@gmail.com>
2021-10-10 04:25:56 -0600

    proyecto creado

---
+++ android/app/src/main/java/com/parcial02_ah132090_ar160127/MainActivity.java
@@ -0,0 +1,15 @@
+package com.parcial02_ah132090_ar160127;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "parcial02_ah132090_ar160127";
+  }
+}
