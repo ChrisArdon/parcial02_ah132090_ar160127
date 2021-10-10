8b9333c53182c3e702984c097f3c592492ebf629
Chris Ardon <rockrisbl@gmail.com>
2021-10-10 05:03:44 -0600

    Navigator añadido a App.js

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -11,11 +11,11 @@ const Tab=createBottomTabNavigator();
 export default function Navigation(){
     return(
         <Tab.Navigator>
-            <Tab.Screen name='suma' component={Sumatoria} options={{title='Suma'}}></Tab.Screen>
-            <Tab.Screen name='resta' component={Resta} options={{title='Resta'}}></Tab.Screen>
-            <Tab.Screen name='multiplicacion' component={Multiplicacion} options={{title='Multiplicacion'}}></Tab.Screen>
-            <Tab.Screen name='division' component={Division} options={{title='Division'}}></Tab.Screen>
-            <Tab.Screen name='factorial' component={Factorial} options={{title='Factorial'}}></Tab.Screen>
+            <Tab.Screen name='suma' component={Sumatoria} options={{title:'Suma'}}></Tab.Screen>
+            <Tab.Screen name='resta' component={Resta} options={{title:'Resta'}}></Tab.Screen>
+            <Tab.Screen name='multiplicacion' component={Multiplicacion} options={{title:'Multiplicacion'}}></Tab.Screen>
+            <Tab.Screen name='division' component={Division} options={{title:'Division'}}></Tab.Screen>
+            <Tab.Screen name='factorial' component={Factorial} options={{title:'Factorial'}}></Tab.Screen>
         </Tab.Navigator>
     );
 }
