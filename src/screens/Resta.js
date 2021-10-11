538d35271ef449f1ed840ca61334b820f4a4c626
Giovanny Abrego <gm.abregoh@gmail.com>
2021-10-10 18:03:41 -0600

    Resta.js completado

--- src/screens/Resta.js
+++ src/screens/Resta.js
@@ -1,10 +1,28 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
-export default function Resta(){
-    return(
+export default function Resta() {
+    const [Num1, guardarNum1] = useState('');
+    const [Num2, guardarNum2] = useState('');
+
+    const Restar = () => {
+        var N1 = parseInt(Num1);
+        var N2 = parseInt(Num2);
+        var R = N1 - N2;
+        alert(R);
+    }
+    return (
         <View>
-            <Text>Estamos en Resta</Text>
+            <TextInput style={styles.input} placeholder='Numero 1' onChangeText={texto => guardarNum1(texto)}></TextInput>
+            <TextInput style={styles.input} placeholder='Numero 2' onChangeText={texto => guardarNum2(texto)}></TextInput>
+            <Button title='Restar' onPress={Restar}></Button>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    input: {
+        borderWidth: 2,
+        margin: 10,
+    },
+});
