e6f034d7ded3a106d6ddba2c0f2a072dfad39115
Giovanny Abrego <gm.abregoh@gmail.com>
2021-10-10 18:12:22 -0600

    Multiplicacion.js completado

--- src/screens/Multiplicacion.js
+++ src/screens/Multiplicacion.js
@@ -1,10 +1,27 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
-export default function Multiplicacion(){
-    return(
+export default function Multiplicacion() {
+    const [Num1, guardarNum1] = useState('');
+    const [Num2, guardarNum2] = useState('');
+    const Multiplicar = () => {
+        var N1 = parseInt(Num1);
+        var N2 = parseInt(Num2);
+        var R = N1 * N2;
+        alert(R);
+    }
+    return (
         <View>
-            <Text>Estamos en Multiplicacion</Text>
+            <TextInput style={styles.input} placeholder='Numero 1' onChangeText={texto => guardarNum1(texto)}></TextInput>
+            <TextInput style={styles.input} placeholder='Numero 2' onChangeText={texto => guardarNum2(texto)}></TextInput>
+            <Button title='Multiplicar' onPress={Multiplicar}></Button>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    input: {
+        borderWidth: 2,
+        margin: 10,
+    },
+});
